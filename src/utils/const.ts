bad5585fc1c39c1adf13490b79567d74e8096781
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2023-11-06 00:06:14 -0500

    Created login page

---
+++ src/utils/const.ts
@@ -0,0 +1,5 @@
+export const ACCESS_TOKEN_KEY = "GROUPME_ACCESS_TOKEN";
+export const PREVIOUS_PAGE_KEY = "PREVIOUS_PAGE";
+
+export const GROUP_ME_AUTH_REDIRECT =
+  "https://oauth.groupme.com/oauth/authorize?client_id=nYOULhOHkuJcPSUjy3TtnXojCWraPk5GcI25nmkm2aShmQ2z";
