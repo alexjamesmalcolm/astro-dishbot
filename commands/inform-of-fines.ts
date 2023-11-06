88ed1c5644ff0b91664a7295495550ab6775b1d9
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2023-11-06 17:15:21 -0500

    Added more to the TODO of inform-of-fines

--- commands/inform-of-fines.ts
+++ commands/inform-of-fines.ts
@@ -2,5 +2,7 @@
 
 /*
 This file is meant for being ran every 10 minutes to inform users of scheduled alerts such as if someone is about to be fined or has already been fined.
+
+There is a date for each rotation which is the "reset" date. If the most recent reset date is too long ago then a reset will be performed as a part of this.
 */
 console.log("hello world");
